4910f2baed753bf5262844150771be4d9127ad0f
crutchtheclutch <william.r.crutchfield@gmail.com>
2024-06-28 23:55:49 -0700

    Update xcode-select.sh

--- scripts/xcode-select.sh
+++ scripts/xcode-select.sh
@@ -35,84 +35,84 @@ get_available_cli_tool_installs() {
 
 info "Validating Xcode CLI tools..."
 
-    # Trick softwareupdate into giving us everything it knows about Xcode CLI tools by
-    # touching the following file to /tmp
-    xclt_tmp="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
-    touch "$xclt_tmp"
+# Trick softwareupdate into giving us everything it knows about Xcode CLI tools by
+# touching the following file to /tmp
+xclt_tmp="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+touch "$xclt_tmp"
 
-    # Run xcrun command to check for a valid Xcode CLI tools path
-    xcrun --version >/dev/null 2>&1
+# Run xcrun command to check for a valid Xcode CLI tools path
+xcrun --version >/dev/null 2>&1
 
-    if [[ "$?" -eq 0 ]]; then
-        info "Valid Xcode CLI tools path found."
+if [[ "$?" -eq 0 ]]; then
+    info "Valid Xcode CLI tools path found."
 
-        # current bundleid for CLI tools
-        bundle_id="com.apple.pkg.CLTools_Executables"
+    # current bundleid for CLI tools
+    bundle_id="com.apple.pkg.CLTools_Executables"
 
-        info "Determining current Xcode CLI version..."
+    info "Determining current Xcode CLI version..."
 
-        if pkgutil --pkgs="$bundle_id" >/dev/null; then
-            # If the CLI tools pkg bundle is found, get the version
-            installed_version=$(pkgutil --pkg-info="com.apple.pkg.CLTools_Executables" | awk '/version:/ {print $2}' | awk -F "." '{print $1"."$2}')
-            ok "Installed Xcode CLI tools version is \"$installed_version\""
-
-        else
-            warn "Unable to determine installed Xcode CLI tools version from \"$bundle_id\"."
-        fi
-
-        info "Checking to see if there are any available Xcode CLI tool updates..."
-
-        # Get the latest available CLI tools
-        cmd_line_tools=("$(get_available_cli_tool_installs)")
+    if pkgutil --pkgs="$bundle_id" >/dev/null; then
+        # If the CLI tools pkg bundle is found, get the version
+        installed_version=$(pkgutil --pkg-info="com.apple.pkg.CLTools_Executables" | awk '/version:/ {print $2}' | awk -F "." '{print $1"."$2}')
+        ok "Installed Xcode CLI tools version is \"$installed_version\""
 
     else
-        warn "Valid Xcode CLI tools path was not found..."
-        info "Getting the latest Xcode CLI tools available for install..."
-
-        # Get the latest available CLI tools
-        cmd_line_tools=("$(get_available_cli_tool_installs)")
+        warn "Unable to determine installed Xcode CLI tools version from \"$bundle_id\"."
     fi
 
-    # if something is returned from the cli tools check
-    if [[ -n $cmd_line_tools ]]; then
-        info "Available Xcode CLI tools found: $cmd_line_tools"
+    info "Checking to see if there are any available Xcode CLI tool updates..."
 
-        if (($(grep -c . <<<"${cmd_line_tools}") > 0)); then
-            cmd_line_tools_output="${cmd_line_tools}"
-            cmd_line_tools=$(echo "${cmd_line_tools_output}" | tail -1)
+    # Get the latest available CLI tools
+    cmd_line_tools=("$(get_available_cli_tool_installs)")
 
-            # get version number of the latest CLI tools installer.
-            lastest_available_version=$(echo "$cmd_line_tools_output" | tail -1 | awk -F "-" '{print $2}')
-        fi
+else
+    warn "Valid Xcode CLI tools path was not found..."
+    info "Getting the latest Xcode CLI tools available for install..."
 
-        if [[ -n $installed_version ]]; then
-            # If an installed CLI tools version is returned
+    # Get the latest available CLI tools
+    cmd_line_tools=("$(get_available_cli_tool_installs)")
+fi
 
-            # compare latest version to installed version using is-at-least
-            version_check="$(is-at-least "$lastest_available_version" "$installed_version" &&
-                echo "greater than or equal to" || echo "less than")"
+# if something is returned from the cli tools check
+if [[ -n $cmd_line_tools ]]; then
+    info "Available Xcode CLI tools found: $cmd_line_tools"
 
-            if [[ $version_check == *"less"* ]]; then
-                # if the installed version is less than available
-                info "Updating $cmd_line_tools..."
-                softwareupdate --install "${cmd_line_tools}" --verbose
+    if (($(grep -c . <<<"${cmd_line_tools}") > 0)); then
+        cmd_line_tools_output="${cmd_line_tools}"
+        cmd_line_tools=$(echo "${cmd_line_tools_output}" | tail -1)
 
-            else
-                # if the installed version is greater than or equal to latest available
-                ok "Installed version \"$installed_version\" is $version_check the latest available version \"$lastest_available_version\"!"
-            fi
+        # get version number of the latest CLI tools installer.
+        lastest_available_version=$(echo "$cmd_line_tools_output" | tail -1 | awk -F "-" '{print $2}')
+    fi
 
-        else
-            info "Installing $cmd_line_tools..."
+    if [[ -n $installed_version ]]; then
+        # If an installed CLI tools version is returned
+
+        # compare latest version to installed version using is-at-least
+        version_check="$(is-at-least "$lastest_available_version" "$installed_version" &&
+            echo "greater than or equal to" || echo "less than")"
+
+        if [[ $version_check == *"less"* ]]; then
+            # if the installed version is less than available
+            info "Updating $cmd_line_tools..."
             softwareupdate --install "${cmd_line_tools}" --verbose
-            ok "Successfully installed Xcode CLI tools!"
+
+        else
+            # if the installed version is greater than or equal to latest available
+            ok "Installed version \"$installed_version\" is $version_check the latest available version \"$lastest_available_version\"!"
         fi
 
     else
-        warn "Hmmmmmm...unabled to return any available CLI tools..."
-        warn "May need to validate the softwareupdate command used."
+        info "Installing $cmd_line_tools..."
+        softwareupdate --install "${cmd_line_tools}" --verbose
+        ok "Successfully installed Xcode CLI tools!"
     fi
 
-    info "Cleaning up $xclt_tmp..."
-    rm "${xclt_tmp}"
-    ok "Successfully removed $xclt_tmp"
+else
+    warn "Hmmmmmm...unabled to return any available CLI tools..."
+    warn "May need to validate the softwareupdate command used."
+fi
+
+info "Cleaning up $xclt_tmp..."
+rm "${xclt_tmp}"
+ok "Successfully removed $xclt_tmp"
