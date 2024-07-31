33a7ec8eebb74f60f4489105442d008027a1e2e7
William Crutchfield <william.r.crutchfield@gmail.com>
2024-07-31 13:18:30 -0700

    revamp

--- init.sh
+++ init.sh
@@ -1,13 +1,20 @@
 #!/bin/zsh
-# NOTE: Utilize zsh rather than bash due to issues with sourcing scripts
+# TODO: add bash support
 
-# This script only supports macOS for the moment, may support linux in the future
+# Use external logging utility script
+#LOG_URL="https://raw.githubusercontent.com/CrutchTheClutch/.dotfiles/HEAD/scripts/log.sh"
+#LOG_SCRIPT=$(mktemp)
+#curl -s $LOG_URL -o $LOG_SCRIPT
+#source $LOG_SCRIPT
+#rm $LOG_SCRIPT
+if [[ -z "$LOG_FUNCTIONS_LOADED" ]]; then
+    LOG_SCRIPT_URL="https://raw.githubusercontent.com/CrutchTheClutch/.dotfiles/HEAD/scripts/log.sh"
+    curl -s "$LOG_SCRIPT_URL" -o /tmp/log.sh
+    source /tmp/log.sh
+    export LOG_FUNCTIONS_LOADED=true
+fi
 
-# Logging helper functions
-# NOTE: Needs to remain a remote url since this script may get executed outside of the .dotfiles repo
-source <(curl -s https://raw.githubusercontent.com/CrutchTheClutch/.dotfiles/HEAD/scripts/init-log.sh)
-
-# 1: Get system information to determine how to proceed
+# Get system information to determine how to proceed
 # TODO: add linux support
 os="$(uname -s)"
 cpu="$(sysctl -n machdep.cpu.brand_string)"
@@ -15,54 +22,63 @@ year=$(sw_vers -buildVersion | cut -c 1,2)
 
 case $os in
   Linux*)
-      fail "Unsupported Linux distribution: ${os}"
-    ;;
+    fail "Unsupported Linux distribution: ${os}"
+  ;;
 
   Darwin*)
     ok "Supported operating system: macOS"
-    ;;
+  ;;
+  
   *)
     fail "Unsupported operating system: ${os}"
-    ;;
+  ;;
 esac
 
-# 2: Install macOS dev tools
-# NOTE: Needs to remain a remote url since this script may get executed outside of the .dotfiles repo
-source <(curl -s https://raw.githubusercontent.com/CrutchTheClutch/.dotfiles/HEAD/scripts/xcode-select.sh)
+# Get sudo access to install all neccesary components
+info "This script requires sudo access in order to bootstrap."
+sudo -v
+# TODO: Handle when user trys to bypass password prompt (fail)
+ok "Sudo access verified!"
 
-# 3: Set macOS preferences
-# NOTE: Needs to remain a remote url since this script may get executed outside of the .dotfiles repo
-source <(curl -s https://raw.githubusercontent.com/CrutchTheClutch/.dotfiles/HEAD/scripts/macos.sh)
-
-# 4: Checkout .dotfile repo at user home directory
-cd $HOME
-
-# if directory exists, warn but continue
-if [ -d "$HOME/.dotfiles" ]; then
-  warn "Dotfiles already exist, local copy may not be up to date with the latest."
-else
-  info "Downloading dotfiles from git clone https://github.com/CrutchTheClutch/.dotfiles.git"
-  git clone https://github.com/CrutchTheClutch/.dotfiles.git
-  cd ~/.dotfiles
-fi
-
-ok "Dotfiles checkedout locally!"
-
-# 5: Install HomeBrew
+# Install Homebrew
 info "Validating Homebrew..."
-
 if ! which brew > /dev/null 2>&1; then
-    info "Homebrew not found, installing now..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+  info "Homebrew not found, installing..."
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+  # Add homebrew to path on Apple Silicon machines
+  if [[ cpu == *"Apple"* ]]; then
+    (echo; echo 'PATH="/opt/homebrew/bin:$PATH"') >> ~/.zshrc
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
+fi
 ok "Homebrew installed!"
 
-# 6: Update HomeBrew
+# Verify Homebrew
+brew doctor
+ok "Homebrew validated!"
+
+# Update Homebrew
 info "Updating Homebrew..."
 brew update
-ok "Homebrew up to date!"
+ok "Homebrew updated!"
 
-info "Installing Ansible..."
+# Upgrade Homebrew
+info "Upgrading Homebrew packages..."
+brew upgrade
+ok "Homebrew packages upgraded!"
+
+# Install Ansible
 brew install ansible
-ok "Ansible installed!"
+
+# Checkout .dotfiles repo and run ansible
+if [ -d "$HOME/.dotfiles" ]; then
+  warn "~/.dotfiles already exists.  Local copy may not be up to date with the latest."
+else 
+  info "Cloning .dotfiles repo..."
+  cd $HOME
+  git clone https://github.com/CrutchTheClutch/.dotfiles.git
+fi
+
+cd $HOME/.dotfiles
+
